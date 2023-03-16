2b74c953fa61d75832fcc56e7aa8bbd7181ef9f6
CarlosChiva <cxhiva@gmail.com>
2023-03-16 20:14:35 +0100

    falta ajustar y probar cacnvas

--- app/src/main/java/com/example/tfg/views/stadisticsViewsFragments/DayFragment.kt
+++ app/src/main/java/com/example/tfg/views/stadisticsViewsFragments/DayFragment.kt
@@ -12,6 +12,7 @@ import com.example.tfg.R
 import com.example.tfg.controllers.SQLController
 import com.example.tfg.controllers.StadisticAdapter
 import com.example.tfg.databinding.FragmentDayBinding
+import com.example.tfg.models.Painter
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
@@ -34,48 +35,60 @@ class DayFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
                val recyclerView=binding.reciclerDay
                recyclerView.layoutManager = LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
-       recyclerView.adapter = StadisticAdapter(this.context!!, demoList())
-
-        val chart: LineChart = binding.lineStadistics
-
-// Configurar las propiedades de la vista de la gráfica
-
-// Configurar las propiedades de la vista de la gráfica
-        chart.xAxis.textSize = 12f
-              chart.axisLeft.textSize = 12f
-        chart.description.isEnabled = false
-        chart.setTouchEnabled(true)
-        chart.isDragEnabled = true
-        chart.setScaleEnabled(true)
-        chart.setPinchZoom(true)
-        chart.setDrawGridBackground(false)
-        chart.setBackgroundColor(Color.WHITE)
-
-// Configurar los datos de la gráfica
-
-// Configurar los datos de la gráfica
-        val entries: MutableList<Entry> = ArrayList()
-        entries.add(Entry(0f,100f))
-        entries.add(Entry(1f, 150f))
-        entries.add(Entry(2f, 120f))
-        entries.add(Entry(3f, 200f))
-        entries.add(Entry(4f, 180f))
-        entries.add(Entry(5f, 220f))
-        entries.add(Entry(6f, 190f))
-        val dataSet = LineDataSet(entries, "Nivel de Glucosa")
-        dataSet.color = Color.RED
-
-        dataSet.setCircleColor(Color.RED)
-        dataSet.circleRadius = 5f
-        dataSet.lineWidth = 2f
-        dataSet.valueTextSize = 10f
-        val lineData = LineData(dataSet)
+               recyclerView.adapter = StadisticAdapter(this.context!!, demoList())
+               val painter=binding.painter
 
 
-// Agregar los datos de la gráfica a la vista de la gráfica
-        chart.data = lineData
-// Actualizar la vista de la gráfica
-        chart.invalidate()
+
+
+
+
+
+
+
+
+
+    //
+//        val chart: LineChart = binding.lineStadistics
+//
+//// Configurar las propiedades de la vista de la gráfica
+//
+//// Configurar las propiedades de la vista de la gráfica
+//        chart.xAxis.textSize = 12f
+//              chart.axisLeft.textSize = 12f
+//        chart.description.isEnabled = false
+//        chart.setTouchEnabled(true)
+//        chart.isDragEnabled = true
+//        chart.setScaleEnabled(true)
+//        chart.setPinchZoom(true)
+//        chart.setDrawGridBackground(false)
+//        chart.setBackgroundColor(Color.WHITE)
+//
+//// Configurar los datos de la gráfica
+//
+//// Configurar los datos de la gráfica
+//        val entries: MutableList<Entry> = ArrayList()
+//        entries.add(Entry(0f,100f))
+//        entries.add(Entry(1f, 150f))
+//        entries.add(Entry(2f, 120f))
+//        entries.add(Entry(3f, 200f))
+//        entries.add(Entry(4f, 180f))
+//        entries.add(Entry(5f, 220f))
+//        entries.add(Entry(6f, 190f))
+//        val dataSet = LineDataSet(entries, "Nivel de Glucosa")
+//        dataSet.color = Color.RED
+//
+//        dataSet.setCircleColor(Color.RED)
+//        dataSet.circleRadius = 5f
+//        dataSet.lineWidth = 2f
+//        dataSet.valueTextSize = 10f
+//        val lineData = LineData(dataSet)
+//
+//
+//// Agregar los datos de la gráfica a la vista de la gráfica
+//        chart.data = lineData
+//// Actualizar la vista de la gráfica
+//        chart.invalidate()
 
     }
     fun demoList():MutableList<String>{
