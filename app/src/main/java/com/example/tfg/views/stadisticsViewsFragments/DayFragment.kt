beccad451f34efded5d2e0133c22b79d5809b1dc
CarlosChiva <cxhiva@gmail.com>
2023-03-17 18:01:25 +0100

    GEneralizado graficos para los distintos fragments, falta ajustar y probarmas las graficas

--- app/src/main/java/com/example/tfg/views/stadisticsViewsFragments/DayFragment.kt
+++ app/src/main/java/com/example/tfg/views/stadisticsViewsFragments/DayFragment.kt
@@ -38,102 +38,104 @@ class DayFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
         recyclerView.adapter = StadisticAdapter(this.context!!, dataPairs)
         val chart: LineChart = binding.lineStadistics
-// Configurar las propiedades de la vista de la gráfica
-        chart.xAxis.labelCount = 5
-        chart.xAxis.valueFormatter = IndexAxisValueFormatter(listOf("00:00", "06:00", "12:00", "18:00", "24:00"))
-        chart.xAxis.textSize = 12f
-        chart.axisLeft.textSize = 12f
-        val yAxis = chart.axisLeft
-        yAxis.axisMinimum = 50f
-        yAxis.axisMaximum = 400f
-        chart.description.isEnabled = false
-        chart.setTouchEnabled(true)
-        chart.isDragEnabled = true
-        chart.setScaleEnabled(true)
-        chart.setPinchZoom(true)
-        chart.xAxis.granularity=5f
-        chart.setDrawGridBackground(false)
-        chart.setBackgroundColor(Color.WHITE)
-
-// Configurar los datos de la gráfica
-        val entries: MutableList<Entry> = ArrayList()
-        for (i in 0 until dataPairs.size) {
-            entries.add(Entry(i.toFloat(), dataPairs[i].second.toFloat()))
-        }
-        val dataSet = LineDataSet(entries, "Nivel de Glucosa")
-        dataSet.color = Color.RED
-        dataSet.setCircleColor(Color.RED)
-        dataSet.circleRadius = 1f
-        dataSet.lineWidth = 1f
-        dataSet.valueTextSize = 10f
-        val lineData = LineData(dataSet)
-
-// Agregar los datos de la gráfica a la vista de la gráfica
-        chart.data = lineData
-
-// Actualizar la vista de la gráfica
-        chart.invalidate()
-        //-----------------------------------------------------------------
-       val pieChart=binding.pieChart
-
-// Contadores para los diferentes rangos de glucosa
-        var above180 = 0
-        var below80 = 0
-        var between = 0
-
-// Calcular la cantidad de tiempo en que la glucosa ha estado en diferentes rangos
-        for (i in 0 until dataPairs.size - 1) {
-            val current = dataPairs[i].second
-            val next = dataPairs[i + 1].second
-            val timeDiff = Duration.between(dataPairs[i].first, dataPairs[i + 1].first).toMinutes()
-
-            if (current > 180 && next > 180) {
-                above180 += timeDiff.toInt()
-            } else if (current < 80 && next < 80) {
-                below80 += timeDiff.toInt()
-            } else {
-                between += timeDiff.toInt()
-            }
-        }
-
-// Crear una lista de entradas para la gráfica de pastel
-        val entriesPie = listOf(
-            PieEntry(above180.toFloat(), "Por encima de 180"),
-            PieEntry(between.toFloat(), "Entre 80 y 180"),
-            PieEntry(below80.toFloat(), "Por debajo de 80")
-        )
-
-// Crear el conjunto de datos y configurar sus propiedades
-        val dataSetPie = PieDataSet(entriesPie, "Tiempo en diferentes rangos de glucosa")
-        dataSetPie.colors = listOf(Color.RED, Color.GREEN, Color.BLUE)
-        dataSetPie.sliceSpace = 3f
-        dataSetPie.selectionShift = 5f
-
-// Crear la instancia de la gráfica de pastel
-        val chartPie = binding.pieChart
-
-// Configurar las propiedades de la vista de la gráfica
-        chartPie.setUsePercentValues(false)
-        chartPie.description.isEnabled = false
-        chartPie.setDrawHoleEnabled(true)
-        chartPie.setHoleColor(Color.WHITE)
-        chartPie.setTransparentCircleColor(Color.WHITE)
-        chartPie.setTransparentCircleAlpha(110)
-        chartPie.holeRadius = 58f
-        chartPie.transparentCircleRadius = 61f
-        chartPie.setDrawCenterText(true)
-        chartPie.rotationAngle = 0f
-        chartPie.isRotationEnabled = true
-        chartPie.isHighlightPerTapEnabled= true
-        chartPie.setEntryLabelColor(Color.BLACK)
-        chartPie.setEntryLabelTextSize(12f)
-
-// Agregar los datos de la gráfica a la vista de la gráfica
-        val data = PieData(dataSetPie)
-        chartPie.data = data
-
-// Actualizar la vista de la gráfica
-        chartPie.invalidate()
+        val pieChart=binding.pieChart
+        val stadisticsGraphics=StadisticsGraphics(requireContext(),chart,pieChart,dataPairs)
+//// Configurar las propiedades de la vista de la gráfica
+//        chart.xAxis.labelCount = 5
+//        chart.xAxis.valueFormatter = IndexAxisValueFormatter(listOf("00:00", "06:00", "12:00", "18:00", "24:00"))
+//        chart.xAxis.textSize = 12f
+//        chart.axisLeft.textSize = 12f
+//        val yAxis = chart.axisLeft
+//        yAxis.axisMinimum = 50f
+//        yAxis.axisMaximum = 400f
+//        chart.description.isEnabled = false
+//        chart.setTouchEnabled(true)
+//        chart.isDragEnabled = true
+//        chart.setScaleEnabled(true)
+//        chart.setPinchZoom(true)
+//        chart.xAxis.granularity=5f
+//        chart.setDrawGridBackground(false)
+//        chart.setBackgroundColor(Color.WHITE)
+//
+//// Configurar los datos de la gráfica
+//        val entries: MutableList<Entry> = ArrayList()
+//        for (i in 0 until dataPairs.size) {
+//            entries.add(Entry(i.toFloat(), dataPairs[i].second.toFloat()))
+//        }
+//        val dataSet = LineDataSet(entries, "Nivel de Glucosa")
+//        dataSet.color = Color.RED
+//        dataSet.setCircleColor(Color.RED)
+//        dataSet.circleRadius = 1f
+//        dataSet.lineWidth = 1f
+//        dataSet.valueTextSize = 10f
+//        val lineData = LineData(dataSet)
+//
+//// Agregar los datos de la gráfica a la vista de la gráfica
+//        chart.data = lineData
+//
+//// Actualizar la vista de la gráfica
+//        chart.invalidate()
+//        //-----------------------------------------------------------------
+//       val pieChart=binding.pieChart
+//
+//// Contadores para los diferentes rangos de glucosa
+//        var above180 = 0
+//        var below80 = 0
+//        var between = 0
+//
+//// Calcular la cantidad de tiempo en que la glucosa ha estado en diferentes rangos
+//        for (i in 0 until dataPairs.size - 1) {
+//            val current = dataPairs[i].second
+//            val next = dataPairs[i + 1].second
+//            val timeDiff = Duration.between(dataPairs[i].first, dataPairs[i + 1].first).toMinutes()
+//
+//            if (current > 180 && next > 180) {
+//                above180 += timeDiff.toInt()
+//            } else if (current < 80 && next < 80) {
+//                below80 += timeDiff.toInt()
+//            } else {
+//                between += timeDiff.toInt()
+//            }
+//        }
+//
+//// Crear una lista de entradas para la gráfica de pastel
+//        val entriesPie = listOf(
+//            PieEntry(above180.toFloat(), "Por encima de 180"),
+//            PieEntry(between.toFloat(), "Entre 80 y 180"),
+//            PieEntry(below80.toFloat(), "Por debajo de 80")
+//        )
+//
+//// Crear el conjunto de datos y configurar sus propiedades
+//        val dataSetPie = PieDataSet(entriesPie, "Tiempo en diferentes rangos de glucosa")
+//        dataSetPie.colors = listOf(Color.RED, Color.GREEN, Color.BLUE)
+//        dataSetPie.sliceSpace = 3f
+//        dataSetPie.selectionShift = 5f
+//
+//// Crear la instancia de la gráfica de pastel
+//        val chartPie = binding.pieChart
+//
+//// Configurar las propiedades de la vista de la gráfica
+//        chartPie.setUsePercentValues(false)
+//        chartPie.description.isEnabled = false
+//        chartPie.setDrawHoleEnabled(true)
+//        chartPie.setHoleColor(Color.WHITE)
+//        chartPie.setTransparentCircleColor(Color.WHITE)
+//        chartPie.setTransparentCircleAlpha(110)
+//        chartPie.holeRadius = 58f
+//        chartPie.transparentCircleRadius = 61f
+//        chartPie.setDrawCenterText(true)
+//        chartPie.rotationAngle = 0f
+//        chartPie.isRotationEnabled = true
+//        chartPie.isHighlightPerTapEnabled= true
+//        chartPie.setEntryLabelColor(Color.BLACK)
+//        chartPie.setEntryLabelTextSize(12f)
+//
+//// Agregar los datos de la gráfica a la vista de la gráfica
+//        val data = PieData(dataSetPie)
+//        chartPie.data = data
+//
+//// Actualizar la vista de la gráfica
+//        chartPie.invalidate()
 
 
 
---
+++ app/src/main/java/com/example/tfg/views/stadisticsViewsFragments/StadisticsGraphics.kt
@@ -0,0 +1,129 @@
+package com.example.tfg.views.stadisticsViewsFragments
+
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import java.time.Duration
+import java.time.LocalDateTime
+
+class StadisticsGraphics(
+    context: Context,
+    viewLineal: LineChart,
+    viewPie: PieChart,
+    list: List<Pair<LocalDateTime, Int>>
+) {
+    val chart: LineChart
+    val pieChart: PieChart
+    val lista: List<Pair<LocalDateTime, Int>>
+
+    init {
+        this.chart = viewLineal
+        this.pieChart = viewPie
+        this.lista = list
+        drawing()
+    }
+
+    fun drawing() {
+// Configurar las propiedades de la vista de la gráfica
+        chart.xAxis.labelCount = 5
+        chart.xAxis.valueFormatter =
+            IndexAxisValueFormatter(listOf("00:00", "06:00", "12:00", "18:00", "24:00"))
+        chart.xAxis.textSize = 12f
+        chart.axisLeft.textSize = 12f
+        val yAxis = chart.axisLeft
+        yAxis.axisMinimum = 50f
+        yAxis.axisMaximum = 400f
+        chart.description.isEnabled = false
+        chart.setTouchEnabled(true)
+        chart.isDragEnabled = true
+        chart.setScaleEnabled(true)
+        chart.setPinchZoom(true)
+        chart.xAxis.granularity = 5f
+        chart.setDrawGridBackground(false)
+        chart.setBackgroundColor(Color.WHITE)
+
+        // Configurar los datos de la gráfica
+        val entries: MutableList<Entry> = ArrayList()
+        for (i in 0 until lista.size) {
+            entries.add(Entry(i.toFloat(), lista[i].second.toFloat()))
+        }
+        val dataSet = LineDataSet(entries, "Nivel de Glucosa")
+        dataSet.color = Color.RED
+        dataSet.setCircleColor(Color.RED)
+        dataSet.circleRadius = 1f
+        dataSet.lineWidth = 1f
+        dataSet.valueTextSize = 10f
+        val lineData = LineData(dataSet)
+
+// Agregar los datos de la gráfica a la vista de la gráfica
+        chart.data = lineData
+
+// Actualizar la vista de la gráfica
+        chart.invalidate()
+        //-----------------------------------------------------------------
+
+
+        // Contadores para los diferentes rangos de glucosa
+        var above180 = 0
+        var below80 = 0
+        var between = 0
+
+// Calcular la cantidad de tiempo en que la glucosa ha estado en diferentes rangos
+        for (i in 0 until lista.size - 1) {
+            val current = lista[i].second
+            val next = lista[i + 1].second
+            val timeDiff = Duration.between(lista[i].first, lista[i + 1].first).toMinutes()
+
+            if (current > 180 && next > 180) {
+                above180 += timeDiff.toInt()
+            } else if (current < 80 && next < 80) {
+                below80 += timeDiff.toInt()
+            } else {
+                between += timeDiff.toInt()
+            }
+        }
+
+        // Crear una lista de entradas para la gráfica de pastel
+        val entriesPie = listOf(
+            PieEntry(above180.toFloat(), "Por encima de 180"),
+            PieEntry(between.toFloat(), "Entre 80 y 180"),
+            PieEntry(below80.toFloat(), "Por debajo de 80")
+        )
+
+        // Crear el conjunto de datos y configurar sus propiedades
+        val dataSetPie = PieDataSet(entriesPie, "Tiempo en diferentes rangos de glucosa")
+        dataSetPie.colors = listOf(Color.RED, Color.GREEN, Color.BLUE)
+        dataSetPie.sliceSpace = 3f
+        dataSetPie.selectionShift = 5f
+
+        // Crear la instancia de la gráfica de pastel
+
+
+// Configurar las propiedades de la vista de la gráfica
+        pieChart.setUsePercentValues(false)
+        pieChart.description.isEnabled = false
+        pieChart.setDrawHoleEnabled(true)
+        pieChart.setHoleColor(Color.WHITE)
+        pieChart.setTransparentCircleColor(Color.WHITE)
+        pieChart.setTransparentCircleAlpha(110)
+        pieChart.holeRadius = 58f
+        pieChart.transparentCircleRadius = 61f
+        pieChart.setDrawCenterText(true)
+        pieChart.rotationAngle = 0f
+        pieChart.isRotationEnabled = true
+        pieChart.isHighlightPerTapEnabled = true
+        pieChart.setEntryLabelColor(Color.BLACK)
+        pieChart.setEntryLabelTextSize(12f)
+
+        // Agregar los datos de la gráfica a la vista de la gráfica
+        val data = PieData(dataSetPie)
+        pieChart.data = data
+
+// Actualizar la vista de la gráfica
+        pieChart.invalidate()
+    }
+}
